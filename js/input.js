2311edbb0e50c51348ac7469d0b7ea0f9681d1da
FadlGh <fadl2009gh@gmail.com>
2023-10-30 11:20:11 +0200

    init db

--- js/input.js
+++ js/input.js
@@ -4,12 +4,12 @@ function AddContainer() {
   test_form.innerHTML += `
     <div class="container">
         <div class="question-holder">
-          <label for="question">Question:</label>
+          <label>Question:</label>
           <input type="text" class="question" placeholder="Enter question" />
         </div>
 
         <div class="answer-holder">
-          <label for="answer">Answer:</label>
+          <label>Answer:</label>
           <input type="text" class="answer" placeholder="Enter answer" />
         </div>
       </div>`;
---
+++ js/input_data.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLlb24UdO4r-MRN5kLU6jxZIE47x-5AH8",
+  authDomain: "flash-cards-12c6a.firebaseapp.com",
+  projectId: "flash-cards-12c6a",
+  storageBucket: "flash-cards-12c6a.appspot.com",
+  messagingSenderId: "607801373750",
+  appId: "1:607801373750:web:f76d8bfbcd5fecd80c5497",
+  measurementId: "G-PNYTN28K2G",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
